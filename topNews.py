c4686f4e24b0e1b534927885c31157d1482f3085
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2020-09-09 17:25:04 +0800

    Add files via upload

---
+++ topNews.py
@@ -0,0 +1,40 @@
+import urllib3
+from bs4 import BeautifulSoup
+
+
+http = urllib3.PoolManager()
+url = 'https://www.channelnewsasia.com/news/singapore'
+response = http.request('GET', url)
+soup_BS = BeautifulSoup(response.data, 'html.parser')
+#print(soup_BS)
+
+ugrid_soup_BS = soup_BS.find_all('div', {'class':"u-grid"})
+
+for t in ugrid_soup_BS:
+    top_stories_BS = t.find('h2', {'class':"section__title"})
+    # print(top_stories_BS)
+
+    if top_stories_BS is None:
+        continue
+    test = top_stories_BS.text.strip().lower()
+
+    if 'top' in test:
+        count = 1
+        top = t.find_all('a', {'class': "teaser__title"})
+        for title in top:
+            if count == 10:
+                break
+            count += 1
+            results = dict()
+            results['title'] = title.text.strip()
+            results['url'] = title.get('href')
+            print(results)
+
+'''
+for title in title_soup_BS:
+    count +=1
+    results = dict()
+    results['title'] = title.text.strip()
+    results['url'] = title.get('href')
+    print(results)
+    '''
